58508439d19da2cf5bf3c936788639e488440d1a
Akash Singh <imfoobar42@gmail.com>
2024-07-19 02:54:37 +0530

    Time: 2 ms (97.23%), Space: 44.7 MB (67.81%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.java
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -1,19 +1,16 @@
 class Solution {
     public int[] numberGame(int[] nums) {
-      int n = nums.length;
-      if(n<2) return nums;
-      Queue<Integer> minHeap = new PriorityQueue<>();
-      for(int val:nums) minHeap.add(val);
-      int i = 0;
-      int[] result = new int[n];
-      while(minHeap.size()>1){ //handling only the even ip
-        int a = minHeap.poll();
-        int b =  minHeap.poll();
-        result[i] = b;
-        result[i+1] = a;
-        i += 2 ;
+        int n = nums.length;
+        if (n < 2) return nums;
+        
+        int[] result = new int[n];
+      Arrays.sort(nums);
+   //   int index =0;
+      for(int i=0 ;i<n; i+=2){
+        result[i] = nums[i+1];
+        result[i+1] = nums[i];
       }
-      return result;
       
+        return result;
     }
 }
