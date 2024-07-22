4c985c5c704fcc7772e5afa6403ee08b8552f30d
Akash Singh <imfoobar42@gmail.com>
2024-07-22 22:47:08 +0530

    Time: 2 ms (16.05%), Space: 44.2 MB (29.65%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,33 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right; 
+ *     }
+ * }
+ */
 class Solution {
-    private List<Integer> DFS(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        
-        class Traverse {
-            Traverse(TreeNode current) {
-                if (current != null) {
-                    if (current.left != null) {
-                        new Traverse(current.left);
-                    }
-                    result.add(current.val);
-                    if (current.right != null) {
-                        new Traverse(current.right);
-                    }
-                }
-            }
+    private void inOrder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
         }
-        
-        if (root != null) {
-            new Traverse(root);
-        }
-        
-        return result;
+        inOrder(root.left, result);
+        result.add(root.val);
+        inOrder(root.right, result);
     }
     
     public boolean isValidBST(TreeNode root) {
         if (root == null) {
             return true;
         }
-        List<Integer> res = DFS(root);
+        List<Integer> res = new ArrayList<>();
+        inOrder(root, res);
         for (int i = 0; i < res.size() - 1; i++) {
             if (res.get(i) >= res.get(i + 1)) {
                 return false;
