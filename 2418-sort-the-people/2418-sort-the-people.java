4599c1793b0b14583faa6ca7b74182228e57c3ca
Akash Singh <imfoobar42@gmail.com>
2024-07-22 18:29:30 +0530

    Time: 7 ms (90.64%), Space: 45.5 MB (18.45%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer,String> map = new HashMap<>();
+        for(int i=0; i<names.length;i++){
+          map.put(heights[i],names[i]);
+        }
+      Arrays.sort(heights);
+      int k=0;
+      for(int j=heights.length-1;j>=0;j--){
+          names[k++] = map.get(heights[j]);
+        }
+      return names;
+    }
+}
