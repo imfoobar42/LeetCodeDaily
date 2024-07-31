6a5f66aaf57aa7e606c5087939504876566b8776
Akash Singh <imfoobar42@gmail.com>
2024-07-31 22:16:17 +0530

    Time: 5 ms (57.33%), Space: 44.9 MB (21.24%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,10 +1,10 @@
 class Solution {
-
   public int romanToInt(String s) {
     int sum = 0;
     int i = 0;
     Map<Character, Integer> map = new HashMap<>();
-    // Symbol Value
+    
+    // Map of Roman numerals to integers
     map.put('I', 1);
     map.put('V', 5);
     map.put('X', 10);
@@ -12,15 +12,14 @@ class Solution {
     map.put('C', 100);
     map.put('D', 500);
     map.put('M', 1000);
-    char[] c = s.toCharArray();
 
     while (i < s.length()) {
-      // Check if next numeral exists and is larger than the current one
-      if (i + 1 < s.length() && map.get(c[i]) < map.get(c[i + 1])) {
-        sum += map.get(c[i + 1]) - map.get(c[i]);
+      // Check if the next numeral exists and is larger than the current one
+      if (i + 1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+        sum += map.get(s.charAt(i + 1)) - map.get(s.charAt(i));
         i += 2; // Skip the next numeral since it's already processed
       } else {
-        sum += map.get(c[i]);
+        sum += map.get(s.charAt(i));
         i++;
       }
     }
