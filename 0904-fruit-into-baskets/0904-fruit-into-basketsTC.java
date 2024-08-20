cf188cf56e91e57564013b1bfe604976b870b236
Akash Singh <imfoobar42@gmail.com>
2024-08-20 16:52:11 -0400

    Update and rename NOTES.md to 0904-fruit-into-basketsTC.java
    Solving in O(2N) -> O(N)
    moving left pointer all the way 
    Can do better by just moving 1 bit and not shrinking the entire window

---
+++ 0904-fruit-into-baskets/0904-fruit-into-basketsTC.java
@@ -0,0 +1,23 @@
+//Solving in O(2n)
+​class Solution {
+  public int totalFruit(int[] fruits) {
+      int r = 0, l =0, maxLen = 0;
+      int n = fruits.length;  
+       Map<Integer,Integer> map = new HashMap<>(); //frequency counter 
+       while(r<n){
+         map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
+         if(map.size()>2){ //if exceeds size 
+          while(map.size()>2){
+            map.put(fruits[l],map.get(fruits[l])-1);
+            if(map.get(fruits[l])==0) //remove fruit at l if count goes to zero
+             map.remove(fruits[l]);        
+            l++;         
+       }
+       }
+      if(map.size()<=2)
+            maxLen = Math.max(maxLen, r-l+1);
+      r++;  
+      }
+      return maxLen;        
+  } 
+}
