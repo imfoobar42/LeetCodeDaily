b5274a11a6cddeb22c952bb116b78d97f320ae6e
Akash Singh <imfoobar42@gmail.com>
2025-01-10 03:58:03 -0500

    Time: 17 ms (18.6%), Space: 53.4 MB (95.93%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.java
+++ 0952-word-subsets/0952-word-subsets.java
@@ -1,28 +1,41 @@
-class Solution{ 
-    public List<String> wordSubsets(String[] A, String[] B) {
-        int[] bmax = count("");
-        for (String b: B) {
-            int[] bCount = count(b);
-            for (int i = 0; i < 26; ++i)
-                bmax[i] = Math.max(bmax[i], bCount[i]);
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+      //creating bMax  
+      //maintain the max frequency of letters in set B
+      int[] bMax = new int[26]; 
+      for(String word : words2){
+        int[] countB_i = countArray(word); //get count array for each word in set B
+        for(int i=0;i<26;i++){
+          bMax[i] = Math.max(countB_i[i],bMax[i]);
         }
-
-        List<String> ans = new ArrayList();
-        search: for (String a: A) {
-            int[] aCount = count(a);
-            for (int i = 0; i < 26; ++i)
-                if (aCount[i] < bmax[i])
-                    continue search;
-            ans.add(a);
-        }
-
-        return ans;
+      }
+      //returning Universal set 
+       List<String> universal_set = new ArrayList<>();
+      
+              
+       //check every word from set A
+       for(String word:words1){
+        if(isUniversal(word,bMax)) universal_set.add(word);
+       }
+       return universal_set;
     }
+    //check if word is universal or not
+    public boolean isUniversal(String words,int[] bMax){
+        int[] word_count = countArray(words);
+        for(int i=0;i<26;i++){
+          if(word_count[i]<bMax[i]) 
+            return false;
+        }
+        return true; //word is a universal_word
+       }
+    //countArray helper function to get frequency of each element in the word
+    public int[] countArray(String s){
+    int[] giveCount = new int[26];
+    for(int i=0;i<s.length();i++){
+      giveCount[s.charAt(i)-'a']++;
+      }
+    return giveCount;
 
-    public int[] count(String S) {
-        int[] ans = new int[26];
-        for (char c: S.toCharArray())
-            ans[c - 'a']++;
-        return ans;
+
     }
 }
