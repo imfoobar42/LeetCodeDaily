04ed46612b3828a9cff17c5f1ac3d82a8c1357f9
Akash Singh <imfoobar42@gmail.com>
2025-01-11 20:03:44 -0500

    Time: 4 ms (87.99%), Space: 45.9 MB (10.71%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -1,22 +1,13 @@
 class Solution {
     public boolean canConstruct(String s, int k) {
-      //get length of string 
-      int n = s.length();
-      if(k>n) return false;
-      if(k==n) return true;
-      //Possible to construct k palindromes only if the odd count characters < k 
-      //maintain the odd count array 
-      int[] countArray = new int[26];
-      int oddCount = 0;
-      //Maintain the frequency of characters in O(n)
-      for(int i=0;i<n;i++){
-        countArray[s.charAt(i)-'a']++;
-      }
-      //check the occurencies of odd characters
-      for(int j=0;j<26;j++){
-        if(countArray[j]%2!=0) 
-          ++oddCount;
-      }
-      return oddCount<=k;
+        int n = s.length();
+        if(k>n) return false; //cant construct when #of palindromes exceed the length of the string
+        if(k==n) return true; //every character is a palindrome in itself
+
+        //Solving with bit manipulation 
+        int bitMask = 0; //a bit mask of 32 bit
+        for(int i=0;i<n;i++)
+          bitMask ^= (1<<(s.charAt(i)-'a'));
+      return Integer.bitCount(bitMask)<=k;
     }
 }
