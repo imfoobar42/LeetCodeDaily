6a5bc45365b635a3f363c8733617e06a717b1502
Akash Singh <imfoobar42@gmail.com>
2025-01-11 20:08:12 -0500

    Time: 2 ms (100%), Space: 45.5 MB (43.51%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -3,7 +3,7 @@ class Solution {
         int n = s.length();
         if(k>n) return false; //cant construct when #of palindromes exceed the length of the string
         if(k==n) return true; //every character is a palindrome in itself
-
+        if(k>=26) return true; //maxm 26 odd characters and even pairs of extra characters
         //Solving with bit manipulation 
         int bitMask = 0; //a bit mask of 32 bit
         for(int i=0;i<n;i++)
