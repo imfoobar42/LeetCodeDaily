570733cbba7a52ee4a42a6a6e2999276ddf235a4
Akash Singh <imfoobar42@gmail.com>
2025-01-17 01:14:11 -0500

    Time: 10 ms (88.54%), Space: 58.3 MB (41.02%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        if(n==1) return false;
+        Set<Integer> set = new HashSet<>();
+        for(int num:nums){
+          if(set.contains(num)) return true;
+          set.add(num);
+        }
+        return false;
+    }
+}
