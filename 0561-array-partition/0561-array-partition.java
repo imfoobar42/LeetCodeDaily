c48a6c9a79b91e7ff5c75eef15a726881f282127
Akash Singh <imfoobar42@gmail.com>
2025-01-18 23:28:20 -0500

    Time: 12 ms (96.62%), Space: 46.9 MB (32.9%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -4,7 +4,7 @@ class Solution {
         //if(n==2) return Math.min(nums[0],nums[1]);
         Arrays.sort(nums);
         int result = 0;
-        for(int i =0;i<n-1;i=i+2){
+        for(int i =0;i<n;i=i+2){
           result += nums[i];
         }
         return result; //12 25 66 = 1+2+6
