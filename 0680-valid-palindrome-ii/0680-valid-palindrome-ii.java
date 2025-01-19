488bd71db5038bc02274d8530546d50ce1190e02
Akash Singh <imfoobar42@gmail.com>
2025-01-19 03:33:43 -0500

    Time: 5 ms (23.14%), Space: 45.2 MB (85.5%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean validPalindrome(String s) {
         //isPalindrome
-        if(checkPalindrome(s, 0, s.length()-1)) return true;   
         int i=0,j =s.length()-1;
         while(i<j){
-          if(s.charAt(i)!=s.charAt(j)) return checkPalindrome(s, i+1, j)||checkPalindrome(s, i, j-1);  
-        i++;
-        j--;
+          if(s.charAt(i)!=s.charAt(j)) 
+          return checkPalindrome(s, i+1, j)|| checkPalindrome(s, i, j-1);  
+          i++;
+          j--;
         }
         return true;
     }
