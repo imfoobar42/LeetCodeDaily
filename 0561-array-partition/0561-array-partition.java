b41a1b27926b5e9b8e6ac0bf9a291ee393cdfd79
Akash Singh <imfoobar42@gmail.com>
2025-01-18 23:27:23 -0500

    Time: 12 ms (96.62%), Space: 46.7 MB (46.04%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int n = nums.length;
+        //if(n==2) return Math.min(nums[0],nums[1]);
+        Arrays.sort(nums);
+        int result = 0;
+        for(int i =0;i<n-1;i=i+2){
+          result += nums[i];
+        }
+        return result; //12 25 66 = 1+2+6
+    }  
+  }
