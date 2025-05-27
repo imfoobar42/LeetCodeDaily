452cee077346e0375848a0325fc79b82007cccdf
Akash Singh <imfoobar42@gmail.com>
2025-05-27 01:29:34 -0400

    Time: 1 ms (98.77%), Space: 42.9 MB (58.92%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -1,17 +1,10 @@
 class Solution {
-    boolean even(int number){
-      int digitCount = 0;
-      // while(number>0){
-      //   digitCount++;
-      //   number /= 10;
-      // }
-      digitCount = (int)Math.log10(number)+1;
-      return digitCount%2==0;
-    }
     public int findNumbers(int[] nums) {
         int count = 0;
-        for(int n:nums){
-          if(even(n)) count++;
+        for(int n : nums){
+          count += ((int) Math.log10(n) & 1); 
+          //log to find the digit count
+          //And to find if digit is odd or even based on it's LSB
         }
         return count;
     }
