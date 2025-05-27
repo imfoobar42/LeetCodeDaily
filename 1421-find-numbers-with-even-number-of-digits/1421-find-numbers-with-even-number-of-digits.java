1e01ed72bf19dcadec7f66e17b0afff2f45e7274
Akash Singh <imfoobar42@gmail.com>
2025-05-27 01:39:58 -0400

    Update 1421-find-numbers-with-even-number-of-digits.java

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -3,6 +3,10 @@ class Solution {
         int count = 0;
         for(int n:nums){
           count += (int)Math.log10(n) & 1;
+            //beautiful solution to the problem 
+            //log10 returns always n-1 digits
+            // anding with 1 checks for the odd value
+            //but in actuality we are checking for even numbered digits only
         }
         return count;
     }
