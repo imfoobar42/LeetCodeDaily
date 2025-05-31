2f395fd7543dfe5f2797f48fcd628bf3c7a3826b
Akash Singh <imfoobar42@gmail.com>
2025-05-31 18:32:48 -0400

    Time: 0 ms (100%), Space: 46.4 MB (8.54%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,27 +1,23 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-      if(nums.length==0) return new int[]{-1,-1};
-      //int first_occurence =-1, last_occurence = -1;
-      boolean first = true;
-      int first_occurence = binarySearch(nums,target,first);
-      int last_occurence = binarySearch(nums,target,!first);
-      return new int[]{first_occurence, last_occurence};
+        if(nums.length==0) return new int[]{-1,-1};
+        int firstOccurence = binarySearch(nums, target, true);
+        int lastOccurence = binarySearch(nums, target, false);
+        return new int[]{firstOccurence, lastOccurence};
     }
-    private int binarySearch(int[] nums, int target,boolean first){
+    static int binarySearch(int[] arr, int target, boolean first){
       int start = 0;
+      int end = arr.length-1;
       int res = -1;
-      int end = nums.length - 1;
       while(start<=end){
-        int mid = start + (end-start)/2;
-        if(nums[mid]==target){
-          res = mid; //update res everytime we find it match with target and store the index in res
-          if(!first){
-             start= mid+1; //finding last occurence
-             }
-          else end=mid-1; //finding first occurrence
+        int mid = start + (end - start)/2;
+        if(arr[mid]==target){ 
+          res = mid;
+          if(first) end = mid-1;
+          else start=mid+1;
         }
-        else if(nums[mid]<target) start = mid+1;
-        else end = mid-1;
+        else if(arr[mid]<target) start=mid+1;
+        else end=mid-1;
       }
       return res;
     }
