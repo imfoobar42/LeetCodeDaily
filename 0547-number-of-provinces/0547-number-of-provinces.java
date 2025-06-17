1d34824cc553b2a9d76966cd1018f7ac1a5a4261
Akash Singh <imfoobar42@gmail.com>
2025-06-17 13:05:20 -0400

    Time: 1 ms (88.49%), Space: 47.6 MB (66.43%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -7,20 +7,20 @@ class Solution {
         }
       }
     }
-    private static void bfs(int i, int[][] isConnected, boolean[] visited){
-      Queue<Integer> queue = new LinkedList<>();
-      visited[i]= true;
-      queue.add(i);
-      while(!queue.isEmpty()){
-      int node = queue.poll();
-        for(int adj=0;adj<isConnected[0].length;adj++){
-          if(isConnected[node][adj]==1 && !visited[adj]){
-            queue.add(adj); //node added to queue as it was not visited 
-            visited[adj]= true; 
-          }
-        }
-      }
-    }
+    // private static void bfs(int i, int[][] isConnected, boolean[] visited){
+    //   Queue<Integer> queue = new LinkedList<>();
+    //   visited[i]= true;
+    //   queue.add(i);
+    //   while(!queue.isEmpty()){
+    //   int node = queue.poll();
+    //     for(int adj=0;adj<isConnected[0].length;adj++){
+    //       if(isConnected[node][adj]==1 && !visited[adj]){
+    //         queue.add(adj); //node added to queue as it was not visited 
+    //         visited[adj]= true; 
+    //       }
+    //     }
+    //   }
+    // }
     public int findCircleNum(int[][] isConnected) {
         //given nxn matrix 
         //need to find no of connected components 
