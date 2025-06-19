a934ef2aa6c107e9df74469581168c472171d03d
Akash Singh <imfoobar42@gmail.com>
2025-06-19 17:48:09 -0500

    Time: 199 ms (34.48%), Space: 12.5 MB (90.97%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def findRestaurant(self, list1, list2):
+        """
+        :type list1: List[str]
+        :type list2: List[str]
+        :rtype: List[str]
+        """
+        # common strings 
+        # with minm sum of their indexes 
+        # hashmap storing indexes and sum 
+        # create a list for result 
+        result = []
+        minIndex = float('inf')
+        map1 = {value : index for index,value in enumerate(list1)}
+
+        for index,value in enumerate(list2):
+          if value in map1.keys():
+            _sum = index + map1[value]
+            if(_sum==minIndex):
+              result.append(value)
+            elif _sum<minIndex:
+              result=[]
+              result.append(value)
+              minIndex = _sum
+        return result
