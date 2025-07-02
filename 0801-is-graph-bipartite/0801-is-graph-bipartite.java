15cc828d4e683b77f350834390761b8420eb7942
Akash Singh <imfoobar42@gmail.com>
2025-07-02 17:53:26 -0500

    Time: 0 ms (100%), Space: 45.3 MB (28.57%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,21 +1,13 @@
 class Solution {
-  private boolean bfs(int srcNode, int parentNode, int[][] edges, int[] color) {
-
-    color[srcNode]=0;
-    Queue<Integer> queue = new LinkedList<>();
-    queue.offer(srcNode);
-
-    while (!queue.isEmpty()) {
-      int currentNode = queue.poll();
-
-      for (int adjacentNode : edges[currentNode]) {
-        if (color[adjacentNode] == -1) {
-          color[adjacentNode] = 1 - color[currentNode];
-          queue.add(adjacentNode);
-        } else if (color[adjacentNode] == color[currentNode])
-          return false;
-      }
+  private boolean dfs(int srcNode, int color,int[][] edges, int[] colors) {
+    colors[srcNode] = color;
+    for (int adjacentNode : edges[srcNode]) {
+      if (colors[adjacentNode] == -1) {
+        if(!dfs(adjacentNode,1-color, edges, colors)) return false;
+      } else if (colors[adjacentNode] == colors[srcNode])
+        return false;
     }
+
     return true;
   }
 
@@ -23,8 +15,8 @@ class Solution {
     int[] colors = new int[edges.length];
     Arrays.fill(colors, -1); // set all color to be -1
     // will visit each of the vertices and set the color
-    for (int i=0;i<edges.length;i++) {
-      if (colors[i]==-1 && !bfs(i, -1, edges, colors))
+    for (int i = 0; i < edges.length; i++) {
+      if (colors[i] == -1 && !dfs(i,0,edges, colors))
         return false;
     }
     return true;
