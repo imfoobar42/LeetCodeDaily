ec993e1f3a60ca19694b4b9361f35cb9d53c27ce
Akash Singh <imfoobar42@gmail.com>
2025-07-24 22:40:43 -0500

    Time: 0 ms (100%), Space: 12.3 MB (100%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
@@ -4,14 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        max_ = float('-inf')
-        seen = set()
-        for num in nums:
-          if num > 0:
-            seen.add(num)
-          if num > max_:
-            max_ = num
+        max_ = max(nums)
         if max_ < 0:
           return max_
+        seen = set(num for num in nums if num>0)
         return sum(seen)
         
