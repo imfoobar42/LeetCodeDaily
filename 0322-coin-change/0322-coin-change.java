34771265f76d16c76d7bbe5b3b5e56c8b54c033c
Akash Singh <imfoobar42@gmail.com>
2025-09-08 16:36:25 -0400

    Time: 10 ms (99.75%), Space: 44.2 MB (90.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,14 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[13][10001];
-        int n = coins.length;
-        //base condition
-        for(int j=1;j<amount+1;j++) dp[0][j]= 10001; //infinite coins 
-        for(int i=0;i<n+1;i++) dp[i][0]=0; //when amount is 0 
+        int[] dp=new int[amount+1];
+        for(int j=0;j<amount+1;++j)dp[j]=amount+1;
+        dp[0]=0; //base case 
 
-        for(int i=1;i<n+1;i++){
-          for(int j=1;j<amount+1;j++){
-             //coins value is less than amount 
-            if(coins[i-1]<=j){
-            //include or exclude 
-            dp[i][j] = Math.min(dp[i-1][j], 1+dp[i][j-coins[i-1]]);
+        for(int i:coins){
+            for(int j=i;j<amount+1;++j){
+                dp[j]=Math.min(dp[j-i]+1,dp[j]);
             }
-            else dp[i][j]= dp[i-1][j]; //dont include at all  
-          }
         }
-        return dp[n][amount]>10000? -1: dp[n][amount];
+        return dp[amount]==amount+1?-1:dp[amount];
     }
 }
