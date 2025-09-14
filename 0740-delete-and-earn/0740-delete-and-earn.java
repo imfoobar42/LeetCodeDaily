7a0898c5c8aeda49f9b8a239ea108753cdb0b6f4
Akash Singh <imfoobar42@gmail.com>
2025-09-13 23:59:14 -0400

    Time: 1 ms (100%), Space: 44.6 MB (66.46%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -5,17 +5,17 @@ class Solution {
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] > max) max = nums[i];
         }
-        int[] bucket = new int[max+1];
+        int[] points = new int[max+1];
 
         for(int num: nums) {
-            bucket[num] += num;
+            points[num] += num;
         }
 
         int[] dp = new int[max+1];
-        dp[0] = bucket[0];
-        dp[1] = bucket[1];
+        dp[0] = points[0];
+        dp[1] = points[1];
         for(int i = 2; i < max+1; i++) {
-            dp[i] = Math.max(bucket[i] + dp[i-2], dp[i-1]);
+            dp[i] = Math.max(points[i] + dp[i-2], dp[i-1]);
         }
         return dp[max];
     }   
