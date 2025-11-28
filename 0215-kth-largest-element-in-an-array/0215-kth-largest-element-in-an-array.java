d32a6b5776c76f1261ffcf849de17f496183fd9d
Akash Singh <imfoobar42@gmail.com>
2025-11-27 19:41:40 -0500

    Time: 45 ms (65.27%), Space: 64.9 MB (43.82%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,13 +1,17 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-       //one way would be to create MaxHeap and poll k times or poll k-1 times and peek once
-      //other efficient way would be to create heap of k size as we go through the input and return the final poll
-      Queue<Integer> minHeap = new PriorityQueue<>();
-      for(int n:nums){
-        minHeap.add(n);
-        if(minHeap.size()>k) minHeap.poll();
-  //At any moment we have heap of size k containing K large elements till now
-      }
-      return minHeap.peek();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); //minHeap to store k large elements encountered
+        //iterate through all elements
+        for(int n:nums){
+          //if current element is greater than 
+          //the root, it can be the kth largest so add it to minHeap
+          if(minHeap.size()<k) minHeap.add(n); //heap size less than k, then keep adding 
+          else if(n>minHeap.peek()){
+            //delete root 
+            minHeap.poll();
+            minHeap.add(n);
+          }
+        }
+        return minHeap.peek();
     }
 }
