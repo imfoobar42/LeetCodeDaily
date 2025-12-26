cca288dfc36a856ed05d1e47ed2d111c60d17a3b
Akash Singh <imfoobar42@gmail.com>
2025-12-26 01:28:35 -0500

    fixed synatax
    Removed shutdown hook for runtime display.

--- 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
@@ -1,13 +1,4 @@
 class Solution {
-  static {
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-        fw.write("0");
-      } catch (Exception e) {
-      }
-    }));
-  }
-
   public int minimumBoxes(int[] apple, int[] capacity) {
     int sum = 0;
     for (int num : apple)
