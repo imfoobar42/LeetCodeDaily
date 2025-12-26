ecb9a9ab57a84a517325c9a0cff6719d8ae3a236
Akash Singh <imfoobar42@gmail.com>
2025-12-26 01:59:31 -0500

    Time: 42 ms (72.64%), Space: 108.9 MB (86.57%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -4,7 +4,6 @@ class Solution {
         Arrays.sort(happiness); //sort the happiness array
         int count = 0; //keep track of children selected
         long res = 0;
-        int currentHappiness = 0; //to track the happiness for each child
         for(int i=n-1;i>=0;i--){
           res += Math.max(0,happiness[i]-count); //minm happiness can go is 0
           count++;
