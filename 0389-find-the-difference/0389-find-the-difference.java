d9dc4355be044f85f61419ff34af8772e63e7ee9
Akash Singh <imfoobar42@gmail.com>
2025-12-28 20:27:44 -0500

    Time: 1 ms (99.79%), Space: 43.3 MB (32.53%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,12 +1,8 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] countArray = new int[26]; //freq map for characters
-        int sLen = s.length();
-        for(int i=0;i<sLen;i++) countArray[s.charAt(i)-'a']++;
-        for(int j=0;j<sLen+1;j++){
-          if(countArray[t.charAt(j)-'a']==0) return t.charAt(j);
-          countArray[t.charAt(j)-'a']--;
-        }
-        return 'a';
+        char ans = 0; 
+        for(char ch:s.toCharArray()) ans^=ch; //xor with s
+        for(char ch:t.toCharArray()) ans^=ch; // xor of ans with t,
+        return ans; //xor removes everything common from s and t 
     }
 }
