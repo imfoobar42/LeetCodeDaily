98cf751b41e464d5411daca224e5f692a3e93704
Akash Singh <imfoobar42@gmail.com>
2025-12-31 00:02:55 -0500

    Time: 7 ms (99.8%), Space: 47.6 MB (78.12%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,16 +1,13 @@
 class NumArray {
-    int[] n;
+    private int[] nums;
     public NumArray(int[] nums) {
-        n = new int[nums.length];
-        n[0] = nums[0];
-        for(int i=1;i<nums.length;i++) {
-            n[i] = n[i-1]+nums[i];
-        }
+        this.nums = nums;
+        for(int i=1;i<nums.length;i++) nums[i]+=nums[i-1];
     }
     
     public int sumRange(int left, int right) {
-        if(left==0) return n[right];
-        return n[right]-n[left-1];
+        if(left==0) return nums[right];
+        return nums[right]-nums[left-1];
     }
 }
 
