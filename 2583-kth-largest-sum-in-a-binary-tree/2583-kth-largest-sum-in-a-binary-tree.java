0a6bd2539633695dd43f229aa50d8748fbe6a317
Akash Singh <imfoobar42@gmail.com>
2026-01-05 22:16:47 -0500

    Time: 47 ms (92.18%), Space: 138.2 MB (91.56%) - LeetHub

---
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.java
@@ -0,0 +1,36 @@
+
+class Solution {
+  public long kthLargestLevelSum(TreeNode root, int k) {
+    //Approach - Level Order Traversal 
+    //min heap of size k to store maxValues
+    //get sum of nodes at each level
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.offer(root); //add the root
+
+    PriorityQueue<Long> minHeap = new PriorityQueue<>();
+
+    while (!queue.isEmpty()) {
+      long levelSum = 0; //current level sum 
+      int qSize = queue.size(); //# of nodes at current level 
+
+      for (int i = 0; i < qSize; i++) {
+        TreeNode currentNode = queue.poll(); //get currentNode 
+        levelSum += currentNode.val;
+
+        if (currentNode.left != null)
+          queue.offer(currentNode.left);
+        if (currentNode.right != null)
+          queue.offer(currentNode.right);
+
+      }
+      minHeap.offer(levelSum); //add the element
+      if (minHeap.size() > k)
+        minHeap.poll();
+
+    }
+    //minHeap size as at every level we add levelSum
+    return minHeap.size() < k ? -1 : minHeap.peek(); //O(1) for peek
+  }
+}
+//TC : O(N + LlogK) - BFS + Heap operations (Offer, Poll)
+//SC : O(N + K) - Queue + O(K) - minHeap
