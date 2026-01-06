01359ed66ba13b09c74f39a0215b279c6733429b
Akash Singh <imfoobar42@gmail.com>
2026-01-06 13:31:48 -0500

    Time: 0 ms (100%), Space: 42.2 MB (29.47%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,13 +1,19 @@
-import java.math.BigInteger;
 class Solution {
   public int numTrees(int n) {
-    //catlan Number 
-    BigInteger catlanNum = BigInteger.ONE;
+    int[] dp = new int[n + 1];
+    //the moment you choose i and fix root 
+    //all 1 to i-1 go on left subtree 
+    //all i+1 to n go on right subtree
+    //Overlapping subproblem 
+    dp[0] = 1; //base case 
 
+    //treat each number as root 
+    //then-> left part * right part 
     for (int i = 1; i <= n; i++) {
-      catlanNum = catlanNum.multiply(BigInteger.valueOf(n + i)).divide(BigInteger.valueOf(i));
+      for (int j = 1; j <= i; j++) {
+        dp[i] += dp[j - 1] * dp[i - j];
+      }
     }
-    catlanNum = catlanNum.divide(BigInteger.valueOf(n+1));
-    return catlanNum.intValue();
+    return dp[n];
   }
 }
