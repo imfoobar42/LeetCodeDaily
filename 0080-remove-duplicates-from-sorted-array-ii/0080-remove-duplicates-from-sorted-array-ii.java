57e110043dc5a41d95590dd03aaa0817ec9e9b14
Akash Singh <imfoobar42@gmail.com>
2026-01-08 14:48:57 -0500

    Time: 0 ms (100%), Space: 48.8 MB (51.61%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,25 +1,13 @@
 class Solution {
   public int removeDuplicates(int[] nums) {
-        //skip when freq of nums[i]>2 
-        //else add it to the nums count 
-        //modify nums
-        //brute force ->
-        //iterate over the array 
-        //map 
-        Map<Integer, Integer> map = new HashMap<>();
-        //num -key
-        //count - value 
-        for(int num:nums){
-          map.put(num, map.getOrDefault(num,0)+1);
-        }
-        int i =0;
-        int j = 0;
-        while(i<nums.length){
-          int count = map.get(nums[i]);
-          nums[j++]= nums[i]; 
-          if(count>=2) nums[j++]=nums[i];
-          i+=count;
-        }
-        return j;
+    int n = nums.length;
+    if (n <= 2)
+      return n;
+    //write points to the index where next element goes 
+    int write = 2;
+    for (int i = 2; i < n; i++) {
+      if(nums[i]!=nums[write-2]) nums[write++] = nums[i];
     }
+    return write;
+  }
 }
