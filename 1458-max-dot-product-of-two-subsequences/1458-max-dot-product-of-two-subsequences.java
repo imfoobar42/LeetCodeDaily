b1406bd6a0b0c858058d04fff5ac64169775ea0d
Akash Singh <imfoobar42@gmail.com>
2026-01-08 01:54:34 -0500

    Time: 10 ms (77.78%), Space: 46.6 MB (31.85%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -1,28 +1,25 @@
 class Solution {
-    public int maxDotProduct(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int[][] dp = new int[m+1][n+1];
-        //to choose or not to choose between the current elements 
-        for(int i=0;i<m+1;i++){
-          for(int j=0;j<n+1;j++){
-            dp[i][j] = Integer.MIN_VALUE;
-          }
-        }
-        for(int i=1;i<m+1;i++){
-          for(int j=1;j<n+1;j++){
-              int product = nums1[i-1]*nums2[j-1];
-              //choice at (i,j)
-              //skip nums1[i-1]
-              //skip nums2[j-1]
-              //prod + take the prev subsequence or not
-              dp[i][j] = Math.max(
-                Math.max(dp[i-1][j],dp[i][j-1]),
-                product + Math.max(0, dp[i-1][j-1])
-              );  
-            }
-          }
-        
-        return dp[m][n];
+  public int maxDotProduct(int[] nums1, int[] nums2) {
+    int m = nums1.length;
+    int n = nums2.length;
+    int[][] dp = new int[m + 1][n + 1];
+    for (int i = 0; i < m + 1; i++)
+      Arrays.fill(dp[i], Integer.MIN_VALUE);
+
+    //to choose or not to choose between the current elements 
+    //choice at (i,j)
+    //skip nums1[i-1]
+    //skip nums2[j-1]
+    //prod + take the prev subsequence or not
+    for (int i = 1; i < m + 1; i++) {
+      for (int j = 1; j < n + 1; j++) {
+        int product = nums1[i - 1] * nums2[j - 1];
+        dp[i][j] = Math.max(
+            Math.max(dp[i - 1][j], dp[i][j - 1]),
+            product + Math.max(0, dp[i - 1][j - 1]));
+      }
     }
+
+    return dp[m][n];
+  }
 }
