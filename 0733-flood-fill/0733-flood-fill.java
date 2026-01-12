c88f3c004826e3afb80664e159e7e19e2d74fe1f
Akash Singh <imfoobar42@gmail.com>
2026-01-11 22:55:26 -0500

    Time: 0 ms (100%), Space: 46.5 MB (90.68%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,19 +1,22 @@
 class Solution {
-    private static void dfs(int[][] image, int sr, int sc, int color, int startingColor){
-      if(sr<0|| sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]==color || image[sr][sc]!=startingColor)
+  private void dfs(int[][] image, int sr, int sc, int color, int startingColor) {
+    if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length ||
+        image[sr][sc] == color || image[sr][sc] != startingColor)
       return;
-      image[sr][sc]= color;
-      dfs(image, sr-1, sc, color,startingColor);
-      dfs(image, sr+1, sc, color,startingColor);
-      dfs(image, sr, sc+1, color, startingColor);
-      dfs(image, sr, sc-1, color, startingColor);
-    }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        //the value in the image itself is a node
-        int startingColor = image[sr][sc];
-        if(startingColor!=color){
-          dfs(image,sr,sc,color, startingColor);
-        }  
-        return image;
-    }
+    //set color of the current node 
+    image[sr][sc] = color;
+    //call dfs on adjacent nodes 
+    dfs(image, sr + 1, sc, color, startingColor);
+    dfs(image, sr, sc + 1, color, startingColor);
+    dfs(image, sr - 1, sc, color, startingColor);
+    dfs(image, sr, sc - 1, color, startingColor);
+  }
+  public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+    //just call dfs on starting node
+    int startingColor = image[sr][sc];
+    if (startingColor != color)
+      dfs(image, sr, sc, color, startingColor);
+    return image;
+  }
+
 }
