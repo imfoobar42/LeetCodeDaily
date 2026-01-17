2ab2930168249d5901e830a1db02230c0eb8d504
Akash Singh <imfoobar42@gmail.com>
2026-01-17 15:05:11 -0500

    Time: 0 ms (100%), Space: 43.2 MB (79.12%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,12 +1,20 @@
 class Solution {
-    public int heightChecker(int[] heights) {
-        //brute force 
-        int[] expected = heights.clone();
-        Arrays.sort(expected);
+  public int heightChecker(int[] heights) {
+//Approach - count sort
+//1.Find the maximum element to get the range of values(k).
+//Create a count array of size max + 1 
+//Count the occurrences of each element in the input array 
+//Calculate the cumulative sum (prefix sum) of the count array. 
+//Build the output array by traversing the original input array in reverse order. 
+
+        int[] countArray = new int[101];
+        for(int height:heights) countArray[height]++;
         int count = 0;
-        int n = heights.length;
-        for(int i=0;i<n;i++){
-          if(heights[i]!=expected[i]) count++;
+        int i=0;
+        for(int height:heights){
+          while(countArray[i]==0) i++;
+          if(height!=i) count++;
+          countArray[i]--;
         }
         return count;
     }
